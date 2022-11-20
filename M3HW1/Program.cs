d80a959c9dee3adab93d2ab8f46b3ef0ee5e0be5
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-11-20 23:32:51 +0200

    add code

--- M3HW1/Program.cs
+++ M3HW1/Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Start start = new Start();
+            start.Run();
         }
     }
 }
