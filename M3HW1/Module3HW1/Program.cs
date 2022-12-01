99a954732ccf9a7592a226c560e601d7ae8453b9
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-12-01 19:57:00 +0200

    add corrected version

---
+++ M3HW1/Module3HW1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Module3HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CustomCollection<int> c = new CustomCollection<int>();
+            int[] testMass = new int[] { 8, 8, 8 };
+            List<int> testC = new List<int> { 9, 9, 9 };
+            for (int i = 0; i < 4; i++)
+            {
+                c.Add(i + 1);
+            }
+            c.AddRange(testMass);
+            c.AddRange(testC);
+            c.RemoveAt(9);
+            c.Remove(3);
+            Console.WriteLine("Values:");
+            foreach (int item in c)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            c.Sort(true);
+            Console.WriteLine("Sorted values:");
+            foreach (int item in c)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
